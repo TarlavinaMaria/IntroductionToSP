a2bd347bbeb0274087c85e8424ef191950f40f5b
Мария Тарлавина <tarlavina50@mail.ru>
2024-02-09 21:20:07 +0700

    coding class

--- Processes/Form1.Designer.cs
+++ Processes/Form1.Designer.cs
@@ -28,20 +28,85 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.richTextBoxProcessName = new System.Windows.Forms.RichTextBox();
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.buttonStop = new System.Windows.Forms.Button();
+            this.labelProcessInfo = new System.Windows.Forms.Label();
+            this.myProcess = new System.Diagnostics.Process();
             this.SuspendLayout();
+            // 
+            // richTextBoxProcessName
+            // 
+            this.richTextBoxProcessName.Location = new System.Drawing.Point(13, 13);
+            this.richTextBoxProcessName.Name = "richTextBoxProcessName";
+            this.richTextBoxProcessName.Size = new System.Drawing.Size(436, 23);
+            this.richTextBoxProcessName.TabIndex = 0;
+            this.richTextBoxProcessName.Text = "calc.exe";
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.Location = new System.Drawing.Point(13, 42);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(75, 23);
+            this.buttonStart.TabIndex = 1;
+            this.buttonStart.Text = "Start";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
+            // 
+            // buttonStop
+            // 
+            this.buttonStop.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonStop.Location = new System.Drawing.Point(374, 42);
+            this.buttonStop.Name = "buttonStop";
+            this.buttonStop.Size = new System.Drawing.Size(75, 23);
+            this.buttonStop.TabIndex = 2;
+            this.buttonStop.Text = "Stop";
+            this.buttonStop.UseVisualStyleBackColor = true;
+            this.buttonStop.Click += new System.EventHandler(this.buttonStop_Click);
+            // 
+            // labelProcessInfo
+            // 
+            this.labelProcessInfo.AutoSize = true;
+            this.labelProcessInfo.Location = new System.Drawing.Point(13, 72);
+            this.labelProcessInfo.Name = "labelProcessInfo";
+            this.labelProcessInfo.Size = new System.Drawing.Size(69, 13);
+            this.labelProcessInfo.TabIndex = 3;
+            this.labelProcessInfo.Text = "Process Info:";
+            // 
+            // myProcess
+            // 
+            this.myProcess.StartInfo.Domain = "";
+            this.myProcess.StartInfo.LoadUserProfile = false;
+            this.myProcess.StartInfo.Password = null;
+            this.myProcess.StartInfo.StandardErrorEncoding = null;
+            this.myProcess.StartInfo.StandardOutputEncoding = null;
+            this.myProcess.StartInfo.UserName = "";
+            this.myProcess.SynchronizingObject = this;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(461, 322);
+            this.Controls.Add(this.labelProcessInfo);
+            this.Controls.Add(this.buttonStop);
+            this.Controls.Add(this.buttonStart);
+            this.Controls.Add(this.richTextBoxProcessName);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBoxProcessName;
+        private System.Windows.Forms.Button buttonStart;
+        private System.Windows.Forms.Button buttonStop;
+        private System.Windows.Forms.Label labelProcessInfo;
+        private System.Diagnostics.Process myProcess;
     }
 }
 
--- Processes/Form1.cs
+++ Processes/Form1.cs
@@ -15,6 +15,58 @@ namespace Processes
         public Form1()
         {
             InitializeComponent();
+            richTextBoxProcessName.Text = "notepad";
+            InitProcess();
+        }
+        void Form1_Closing(object sender, CancelEventArgs e)
+        {
+            myProcess.CloseMainWindow();
+            myProcess.Close();
+        }
+        void InitProcess()
+        {
+            AllignText();
+            myProcess.StartInfo = new System.Diagnostics.ProcessStartInfo(richTextBoxProcessName.Text);
+
+        }
+        void AllignText()
+        {
+            richTextBoxProcessName.SelectAll();
+            richTextBoxProcessName.SelectionAlignment = HorizontalAlignment.Center;
+
+        }
+
+        private void buttonStart_Click(object sender, EventArgs e)
+        {
+            InitProcess();
+            myProcess.Start();
+            Info();
+        }
+
+        private void buttonStop_Click(object sender, EventArgs e)
+        {
+            myProcess.CloseMainWindow();//Закрывает процесс
+            myProcess.Close(); // Освобождает ресурсы занимаемые процессом
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            myProcess.CloseMainWindow();//Закрывает процесс
+            myProcess.Close(); // Освобождает ресурсы занимаемые процессом
+        }
+        void Info()
+        {
+            labelProcessInfo.Text = "Process Info:\n";
+            labelProcessInfo.Text += $"PID:                      {myProcess.Id}\n";
+            labelProcessInfo.Text += $"BasePriority:             {myProcess.BasePriority}\n";
+            labelProcessInfo.Text += $"PriorityClass:            {myProcess.PriorityClass}\n";
+            labelProcessInfo.Text += $"StartTime:                {myProcess.StartTime}\n";
+            labelProcessInfo.Text += $"UserProcessorTime:        {myProcess.UserProcessorTime}\n";
+            labelProcessInfo.Text += $"TotalProcessorTime:       {myProcess.TotalProcessorTime}\n";
+            labelProcessInfo.Text += $"SessionId:                {myProcess.SessionId}\n";
+            labelProcessInfo.Text += $"ProcessName:              {myProcess.ProcessName}\n";
+            labelProcessInfo.Text += $"Threads:                  {myProcess.Threads.Count}\n";
+            labelProcessInfo.Text += $"Threads:                  {myProcess.Threads.Count}\n";
         }
     }
 }
