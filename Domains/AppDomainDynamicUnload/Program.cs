eb1ad1d66dd47b41300b8665d5f388ab4a873f7a
Мария Тарлавина <tarlavina50@mail.ru>
2024-02-16 21:22:25 +0700

    coding class

---
+++ Domains/AppDomainDynamicUnload/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppDomainDynamicUnload
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //1) Создаем домен приложения:
+            AppDomain domain = AppDomain.CreateDomain("Demo");
+
+            //2) Загружаем DLL в домен:
+            Assembly asm = domain.Load(AssemblyName.GetAssemblyName("SampleLibrary.dll"));
+
+            //3) Получаем модуль, из которого будем вызывать функцию:
+            Module module = asm.GetModule("SampleLibrary.dll");
+
+            //4) Получаем тип данных
+            Type type = module.GetType("SampleLibrary.SampleClass");
+
+            //5) Вынимает метод из класса
+            MethodInfo method = type.GetMethod("Print");
+
+            //6) Вызываем метод
+            method.Invoke(null, null);
+        }
+    }
+}
