faf009ff75d26691e9db59e1124df419e2edf872
Мария Тарлавина <tarlavina50@mail.ru>
2024-02-18 22:00:21 +0700

    coding dz

---
+++ IntroductionToSP/Task Manager/Form1.Designer.cs
@@ -0,0 +1,198 @@
+﻿namespace Task_Manager
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.завершитьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.завершитьToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(15, 14);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(88, 25);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Обновить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(110, 14);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(170, 25);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Завершить процесс";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2});
+            this.listView1.ContextMenuStrip = this.contextMenuStrip1;
+            this.listView1.FullRowSelect = true;
+            this.listView1.GridLines = true;
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(13, 64);
+            this.listView1.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.listView1.MultiSelect = false;
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(544, 444);
+            this.listView1.TabIndex = 2;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Название процесса";
+            this.columnHeader1.Width = 200;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Используемая память";
+            this.columnHeader2.Width = 200;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.завершитьToolStripMenuItem,
+            this.завершитьToolStripMenuItem1});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(239, 48);
+            // 
+            // завершитьToolStripMenuItem
+            // 
+            this.завершитьToolStripMenuItem.Name = "завершитьToolStripMenuItem";
+            this.завершитьToolStripMenuItem.Size = new System.Drawing.Size(238, 22);
+            this.завершитьToolStripMenuItem.Text = "Завершить";
+            // 
+            // завершитьToolStripMenuItem1
+            // 
+            this.завершитьToolStripMenuItem1.Name = "завершитьToolStripMenuItem1";
+            this.завершитьToolStripMenuItem1.Size = new System.Drawing.Size(238, 22);
+            this.завершитьToolStripMenuItem1.Text = "Завершить дерево процессов";
+            this.завершитьToolStripMenuItem1.Click += new System.EventHandler(this.завершитьToolStripMenuItem1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(96, 40);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(244, 22);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(24, 40);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 19);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Поиск";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(288, 14);
+            this.button3.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(170, 25);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Завершить дерево процессов";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(466, 13);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(91, 26);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "Добавить";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(579, 520);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.Name = "Form1";
+            this.Text = "Task Manager";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem завершитьToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem завершитьToolStripMenuItem1;
+    }
+}
+
---
+++ IntroductionToSP/Task Manager/Form1.cs
@@ -0,0 +1,221 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.Diagnostics;
+using System.Management;
+using Microsoft.VisualBasic;
+
+namespace Task_Manager
+{
+    public partial class Form1 : Form
+    {
+        private List<Process> processes = null; //список процессов
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void GetProcesses()
+        {
+            processes.Clear();// очищаем список
+
+            processes = Process.GetProcesses().ToList<Process>(); // заполнение списка системными процессами, в формате ToList<Process>()
+        }
+        private void RefreshProcessesList()
+        {
+            listView1.Items.Clear();// получение list
+
+            double memSize = 0; // переменная памяти
+            foreach (Process p in processes)
+            {
+                memSize = 0;
+
+                PerformanceCounter pc = new PerformanceCounter();
+                pc.CategoryName = "Process";
+                pc.CounterName = "Working Set - Private";
+                pc.InstanceName = p.ProcessName;
+
+                memSize = (double)pc.NextValue() / (1000 * 1000); // в мегобайтах
+
+                string[] row = new string[] { p.ProcessName.ToString(), Math.Round(memSize, 1).ToString() }; // массив сток в котором хранятся колонки 
+
+                listView1.Items.Add(new ListViewItem(row)); // добавление в List
+
+                pc.Close();
+                pc.Dispose();
+            }
+            // в шапке формы процессы
+            Text = "Запущено процессов: " + processes.Count.ToString();
+        }
+        private void RefreshProcessesList(List<Process> processes, string keyword)
+        {
+            try
+            {
+                listView1.Items.Clear();// получение list
+
+                double memSize = 0; // переменная памяти
+                foreach (Process p in processes)
+                {
+                    if (p != null)
+                    {
+                        memSize = 0;
+
+                        PerformanceCounter pc = new PerformanceCounter();
+                        pc.CategoryName = "Process";
+                        pc.CounterName = "Working Set - Private";
+                        pc.InstanceName = p.ProcessName;
+
+                        memSize = (double)pc.NextValue() / (1000 * 1000); // в мегобайтах
+
+                        string[] row = new string[] { p.ProcessName.ToString(), Math.Round(memSize, 1).ToString() }; // массив сток в котором хранятся колонки 
+
+                        listView1.Items.Add(new ListViewItem(row)); // добавление в List
+
+                        pc.Close();
+                        pc.Dispose();
+                    }
+                }
+                // в шапке формы процессы
+                Text = $"Запущено процессов: '{keyword}'" + processes.Count.ToString();
+            }
+            catch (Exception) { }
+        }
+
+        private void KillProcess(Process process)
+        {
+            process.Kill();
+
+            process.WaitForExit();
+        }
+        private void KillProcessAndChildren(int pid)//Завершение дочерних процессов
+        {
+            if (pid == 0)
+            {
+                return;
+            }
+            ManagementObjectSearcher searcher = new ManagementObjectSearcher("Select * From Win32_Process Where ParentProcessID =" + pid);
+
+            ManagementObjectCollection objectsCollection = searcher.Get();
+            foreach (ManagementObject obj in objectsCollection)
+            {
+                KillProcessAndChildren(Convert.ToInt32(obj["ProcessID"]));
+            }
+            try
+            {
+                Process p = Process.GetProcessById(pid);
+
+                p.Kill();
+                p.WaitForExit();
+            }
+            catch (ArgumentException) { }
+        }
+        private int GetParentProcessId(Process p)
+        {
+            int parentID = 0;
+            try
+            {
+                ManagementObject managementObject = new ManagementObject("win32_process.handle='" + p.Id + "'");
+                managementObject.Get();
+
+                parentID = Convert.ToInt32(managementObject["ParentProcessId"]);
+            }
+            catch (Exception) { }
+            return parentID;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            processes = new List<Process>();
+            //заполнение список с процессами
+            GetProcesses();
+            //заполнение ListView
+            RefreshProcessesList();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            GetProcesses();
+
+            RefreshProcessesList();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (listView1.SelectedItems[0] != null)
+                {
+                    //where -получим процесс, сравниваем имя процесса с текстом в крайней колонке
+                    Process processToKill = processes.Where((x) => x.ProcessName ==
+                     listView1.SelectedItems[0].SubItems[0].Text).ToList()[0];
+
+                    KillProcess(processToKill);
+                    GetProcesses();
+                    RefreshProcessesList();
+                }
+            }
+            catch (Exception) { }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (listView1.SelectedItems[0] != null)
+                {
+                    //where -получим процесс, сравниваем имя процесса с текстом в крайней колонке
+                    Process processToKill = processes.Where((x) => x.ProcessName ==
+                     listView1.SelectedItems[0].SubItems[0].Text).ToList()[0];
+
+                    KillProcessAndChildren(GetParentProcessId(processToKill));
+                    GetProcesses();
+                    RefreshProcessesList();
+                }
+            }
+            catch (Exception) { }
+        }
+
+        private void завершитьToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (listView1.SelectedItems[0] != null)
+                {
+                    //where -получим процесс, сравниваем имя процесса с текстом в крайней колонке
+                    Process processToKill = processes.Where((x) => x.ProcessName ==
+                     listView1.SelectedItems[0].SubItems[0].Text).ToList()[0];
+
+                    KillProcessAndChildren(GetParentProcessId(processToKill));
+                    GetProcesses();
+                    RefreshProcessesList();
+                }
+            }
+            catch (Exception) { }
+        }
+        private void button4_Click(object sender, EventArgs e)
+        {
+            string path = Interaction.InputBox("Введите имя программы", "Запуск новой задачи");
+            try
+            {
+                Process.Start(path);
+            }
+            catch (Exception) { }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            GetProcesses();
+
+            List<Process> filteredprocesses = processes.Where((x) =>
+            x.ProcessName.ToLower().Contains(textBox1.Text.ToLower())).ToList<Process>();
+
+            RefreshProcessesList(filteredprocesses, textBox1.Text);
+        }
+    }
+}
